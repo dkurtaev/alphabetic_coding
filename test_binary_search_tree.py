1bc81e5a84567408fb1bc02748f08f80e7eae061
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2016-09-22 14:22:02 +0300

    Test for minimal key extraction from BST

--- test_binary_search_tree.py
+++ test_binary_search_tree.py
@@ -1,5 +1,5 @@
 import unittest
-import random
+import numpy as np
 
 from binary_search_tree import BST
 
@@ -7,13 +7,24 @@ from binary_search_tree import BST
 class TestBST(unittest.TestCase):
 
     def test_fuzz(self):
-        random.seed(324)
+        np.random.seed(324)
         tree = BST()
         for i in range(1000):
-            if random.randint(0, 1):
-                tree.add_node(random.random())
+            if np.random.randint(0, 2):
+                tree.add_node(key=np.random.rand())
             else:
                 tree.pop_min_key_node()
+
+    def test_pop_min_key(self):
+        np.random.seed(324)
+        tree = BST()
+        values = np.random.randint(low=0, high=100, size=1000)
+        for value in values:
+            tree.add_node(key=value)
+
+        for value in sorted(values):
+            self.assertEqual(value, tree.pop_min_key_node().key)
+
 
 if __name__ == '__main__':
     unittest.main()
