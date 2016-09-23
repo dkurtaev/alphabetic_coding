9ffe5e9c68b253a00f2a027d924423a3a17f8a19
dkurtaev <dmitry.kurtaev@gmail.com>
2016-09-23 18:57:20 +0300

    Randomly generated test text

--- test_huffman_coding.py
+++ test_huffman_coding.py
@@ -1,30 +1,49 @@
 import unittest
+import string
+import random
 
 from huffman_coding import HuffmanEncoder, HuffmanDecoder
 
 
 class TestHuffmanCoding(unittest.TestCase):
 
+    def gen_text(self):
+        text_length = random.randint(2, 25)
+        return ''.join(random.choice(text_seed) for j in range(text_length))
+
     def test_all_chars_used(self):
-        text = 'my little string'
-        scheme = HuffmanEncoder().get_coding_table(text)
-        for char in text:
-            self.assertIn(char, scheme)
+        for gen in range(1000):
+            text = self.gen_text()
+            scheme = HuffmanEncoder().get_coding_table(text)
+            for char in text:
+                self.assertIn(char, scheme)
 
     def test_codes_are_binary(self):
-        text = 'my little string'
-        scheme = HuffmanEncoder().get_coding_table(text)
-        for code in scheme.values():
-            self.assertRegexpMatches(code, r'^[01]+$')
+        for gen in range(1000):
+            text = self.gen_text()
+            scheme = HuffmanEncoder().get_coding_table(text)
+            for code in scheme.values():
+                self.assertRegexpMatches(code, r'^[01]+$')
 
     def test_coding_are_prefix(self):
-        text = 'my little string'
-        scheme = HuffmanEncoder().get_coding_table(text)
-        codes = scheme.values()
-        for i in range(len(codes) - 1):
-            for j in range(i + 1, len(codes)):
-                self.assertFalse(codes[i].startswith(codes[j]))
-                self.assertFalse(codes[j].startswith(codes[i]))
+        for gen in range(1000):
+            text = self.gen_text()
+            scheme = HuffmanEncoder().get_coding_table(text)
+            codes = scheme.values()
+            for i in range(len(codes) - 1):
+                for j in range(i + 1, len(codes)):
+                    self.assertFalse(codes[i].startswith(codes[j]))
+                    self.assertFalse(codes[j].startswith(codes[i]))
+
+    def test_decoder(self):
+        for gen in range(1000):
+            text = self.gen_text()
+            coding_table = HuffmanEncoder().get_coding_table(text)
+            encoded_text = HuffmanEncoder().encode(text)
+            decoded_text = HuffmanDecoder().decode(encoded_text, coding_table)
+            self.assertEqual(text, decoded_text)
 
 if __name__ == '__main__':
+    random.seed(324)
+    text_seed = ' ' + string.digits + string.letters + string.punctuation
     unittest.main()
