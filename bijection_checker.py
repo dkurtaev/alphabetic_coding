03a89a5613c879b42d64cc74a9c38bfb767da101
dkurtaev <dmitry.kurtaev@gmail.com>
2016-09-25 22:57:43 +0300

    Added test for checker output

--- bijection_checker.py
+++ bijection_checker.py
@@ -2,61 +2,59 @@ from collections import deque
 
 from coding_tree import CodingTree
 
-class BijectionChecker(object):
 
-    def __init__(self, codes):
-        self.codes = codes
-        self.coding_tree = CodingTree()
-        for code in codes:
-            self.coding_tree.add_node(code, code)
+def check_bijection(codes):
+    """Check bijection of coding with passed binary sequences. If coding is not
+    bijective returns two different sets of codes which conctatinations are
+    equals."""
+    coding_tree = CodingTree()
+    for code in codes:
+        coding_tree.add_node(code, code)
 
-    def check(self):
-        return self.find() == None
+    states = deque([State(code, -1, [], [code]) for code in codes])
+    deficits_hashes = set([])
+    while len(states) > 0:
+        state = states.popleft()
+        codes_above_deficit, codes_below_deficit = \
+                coding_tree.get_neighbors(state.deficit)
 
-    def find(self):
-        class State(object):
+        for code in codes_above_deficit + codes_below_deficit:
+            next_state = state.compensate_deficit(code)
 
-            def __init__(self, deficit, sign, upper_word=None, lower_word=None):
-                self.deficit = deficit
-                self.sign = sign
-                self.upper_word = upper_word
-                self.lower_word = lower_word
+            if next_state.deficit != '':
+                if next_state.hash() not in deficits_hashes:
+                    states.append(next_state)
+                    deficits_hashes.add(next_state.hash())
+            elif len(next_state.upper_word) > 1 or \
+                 len(next_state.lower_word) > 1:
+                return False, next_state.upper_word, next_state.lower_word
 
-            def compensate_deficit(self, sequence):
-                if len(sequence) > len(self.deficit):
-                    new_deficit = sequence[len(self.deficit):]
-                    new_sign = -self.sign
-                else:
-                    new_deficit = self.deficit[len(sequence):]
-                    new_sign = self.sign
+    return True, None, None
 
-                if self.sign < 0:
-                    return State(new_deficit, new_sign,
-                                 upper_word=self.upper_word + [sequence],
-                                 lower_word=self.lower_word)
-                else:
-                    return State(new_deficit, new_sign,
-                                 upper_word=self.upper_word,
-                                 lower_word=self.lower_word + [sequence])
+class State(object):
 
-            def hash(self):
-                return self.sign * int('1' + self.deficit, 2)
+    def __init__(self, deficit, sign, upper_word=None, lower_word=None):
+        self.deficit = deficit
+        self.sign = sign
+        self.upper_word = upper_word
+        self.lower_word = lower_word
 
+    def compensate_deficit(self, sequence):
+        if len(sequence) > len(self.deficit):
+            new_deficit = sequence[len(self.deficit):]
+            new_sign = -self.sign
+        else:
+            new_deficit = self.deficit[len(sequence):]
+            new_sign = self.sign
 
-        states = deque([State(code, -1, [], [code]) for code in self.codes])
-        deficits_hashes = set([])
-        while len(states) > 0:
-            state = states.popleft()
-            codes_above_deficit, codes_below_deficit = \
-                    self.coding_tree.get_neighbors(state.deficit)
+        if self.sign < 0:
+            return State(new_deficit, new_sign,
+                         upper_word=self.upper_word + [sequence],
+                         lower_word=self.lower_word)
+        else:
+            return State(new_deficit, new_sign,
+                         upper_word=self.upper_word,
+                         lower_word=self.lower_word + [sequence])
 
-            for code in codes_above_deficit + codes_below_deficit:
-                next_state = state.compensate_deficit(code)
-
-                if next_state.deficit != '':
-                    if next_state.hash() not in deficits_hashes:
-                        states.append(next_state)
-                        deficits_hashes.add(next_state.hash())
-                elif len(next_state.upper_word) > 1 or \
-                     len(next_state.lower_word) > 1:
-                    return next_state.upper_word, next_state.lower_word
+    def hash(self):
+        return self.sign * int('1' + self.deficit, 2)
--- test/test_bijection_checker.py
+++ test/test_bijection_checker.py
@@ -1,6 +1,6 @@
 from test_coding import TestCoding
 from test_code_generator import gen_loop
-from bijection_checker import BijectionChecker
+from bijection_checker import check_bijection
 from huffman_coding import HuffmanEncoder
 
 class TestBijectionChecker(TestCoding):
@@ -12,8 +12,8 @@ class TestBijectionChecker(TestCoding):
     def test_mcmillan_inequality(self):
         """Test that if coding is bijective then mcmillan inequality is true"""
         def test(M, N, L, codes):
-            checker = BijectionChecker(codes)
-            if checker.check():
+            is_bijective, _, _ = check_bijection(codes)
+            if is_bijective:
                 terms = [pow(2, M - len(code)) for code in codes]
                 mcmillan_value = sum(terms)
                 self.assertLessEqual(mcmillan_value, pow(2, M))
@@ -25,5 +25,20 @@ class TestBijectionChecker(TestCoding):
         for _ in range(1000):
             text = self.gen_text()
             coding_table = HuffmanEncoder().get_coding_table(text)
-            checker = BijectionChecker(coding_table.values())
-            self.assertTrue(checker.check())
+            is_bijective, _, _ = check_bijection(coding_table.values())
+            self.assertTrue(is_bijective)
+
+    def test_checker_output(self):
+        """If coding is not bijective, checker returns pair of codes which
+        encoding equally."""
+        def test(M, N, L, codes):
+            is_bijective, first_word, second_word = check_bijection(codes)
+            if not is_bijective:
+                self.assertGreater(len(first_word), 0)
+                self.assertGreater(len(second_word), 0)
+                for code in first_word + second_word:
+                    self.assertIn(code, codes)
+                self.assertNotEqual(first_word, second_word)
+                self.assertEqual(''.join(first_word), ''.join(second_word))
+
+        gen_loop(test)
